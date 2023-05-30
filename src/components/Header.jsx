9f0e85344821a086b7bea45b16f4ddb81e8b5ca2
SirPhoros <cgtorrubia@gmail.com>
2023-05-30 12:20:44 +0100

    Improving FrontPage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
 	return (
 		<>
-			<h1 className="header">Cristobal's NC_News</h1>
+			<h1 className="header">The Hive</h1>
 		</>
 	)
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,15 @@
 export default function Home() {
 	return (
 		<>
-			<h2 className="title">Welcome to NC_News</h2>
-			<p className="description-title">
-				Introducing NC News: Your Source for Timely and Reliable News
+			<h2 className="title">Welcome to The Hive</h2>
+			<h3 className="description-title">
+				Introducing The Hive: Your Source for Timely and Reliable News
+			</h3>
+			<p>
+				Welcome to The Hive, a vibrant online news destination designed by and
+				for the community. Inspired by the collaborative spirit of Reddit and
+				the industrious nature of the working bees from Manchester, The Hive
+				aims to create a buzzing hive of information and engagement.
 			</p>
 		</>
 	)
