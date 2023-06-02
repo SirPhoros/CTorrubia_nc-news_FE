092ca92586d04faad8808618209fbe97dcf0c470
SirPhoros <cgtorrubia@gmail.com>
2023-06-02 11:38:39 +0100

    User validation / Login page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,8 +11,10 @@ export default function Nav() {
 			<Link to="/">Home </Link>
 			<Link to="/articles">Articles </Link>
 			<Link to="/topics">Topics </Link>
-			<Link to="/users">Users </Link>
-			<section className="profile">Welcome: {username} </section>
+
+			<section className="profile">
+				Welcome: <Link to="/users">{username} </Link>{' '}
+			</section>
 		</nav>
 	)
 }
